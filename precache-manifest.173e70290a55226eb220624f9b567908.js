1b38ed351e7b240a84961d9adc29c827bfbe60ca
Fdcoder81 <carloanselmi81@gmail.com>
2019-09-10 12:06:27 +0200

    Updates

---
+++ precache-manifest.173e70290a55226eb220624f9b567908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e55d172d996af4d986beb02608adb796",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6ec5352e349da8f08a8",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0ce99e94a47f0079ee44",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a6ec5352e349da8f08a8",
+    "url": "/static/js/2.b6970e26.chunk.js"
+  },
+  {
+    "revision": "0ce99e94a47f0079ee44",
+    "url": "/static/js/main.9fbaf855.chunk.js"
+  },
+  {
+    "revision": "503fd1c88bd17ca07809",
+    "url": "/static/js/runtime~main.3a5aa531.js"
+  }
+]);
