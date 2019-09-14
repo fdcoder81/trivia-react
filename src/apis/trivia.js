cab8d6072ad48d811120231bf92102a94d74308f
Fdcoder81 <carloanselmi81@gmail.com>
2019-09-14 09:00:11 +0200

    Fixed issue in Api

--- src/apis/trivia.js
+++ src/apis/trivia.js
@@ -1,11 +1,13 @@
 const getQuestions = async (category, difficulty) => {
+  console.log(category);
   const base = "https://opentdb.com/api.php?amount=10";
   let query = "";
 
-  if (category !== 0 && difficulty !== "easy") {
+  if (category !== 0 || difficulty !== "easy") {
     query = `&category=${category}&difficulty=${difficulty}`;
   }
 
+  console.log(query);
   const response = await fetch(base + query);
   const data = await response.json();
   return data.results;
